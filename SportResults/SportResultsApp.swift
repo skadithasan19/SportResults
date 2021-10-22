76b65e6d8af480c70f6e0b7acabfc5083af8ee4e
Md Adit Hasan <skadithasan19@gmail.com>
2021-10-22 14:32:00 -0500

    Initial Commit

---
+++ SportResults/SportResultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportResultsApp.swift
+//  SportResults
+//
+//  Created by Adit Hasan on 10/22/21.
+//
+
+import SwiftUI
+
+@main
+struct SportResultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
